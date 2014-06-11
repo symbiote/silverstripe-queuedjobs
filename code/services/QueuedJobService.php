c79df6611d3d4e0fbdfa5a616325ac90218fdf99
mikeyc7m <mikeyc7m@users.noreply.github.com>
2014-06-11 13:03:21 +1000

    Update QueuedJobService.php
    errcontext expected to be an array, errorwritters throw error

--- code/services/QueuedJobService.php
+++ code/services/QueuedJobService.php
@@ -277,7 +277,7 @@ class QueuedJobService {
 					'errstr' => 'Broken jobs were found in the job queue',
 					'errfile' => __FILE__,
 					'errline' => __LINE__,
-					'errcontext' => ''
+					'errcontext' => array('')
 				), SS_Log::ERR);
 			}
 		}
@@ -422,7 +422,7 @@ class QueuedJobService {
 						'errstr' => 'Job descriptor ' . $jobId . ' could not be found',
 						'errfile' => __FILE__,
 						'errline' => __LINE__,
-						'errcontext' => ''
+						'errcontext' => array('')
 					), SS_Log::ERR);
 					break;
 				}
@@ -472,7 +472,7 @@ class QueuedJobService {
 						'errstr' => 'Job descriptor has been set to null',
 						'errfile' => __FILE__,
 						'errline' => __LINE__,
-						'errcontext' => ''
+						'errcontext' => array('')
 					), SS_Log::WARN);
 					$broken = true;
 				}
