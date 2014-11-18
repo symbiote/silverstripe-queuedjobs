cc4c73d44a220a17c5b407d82cb8079b536fbf38
matt-in-a-hat <matt.lang.42@gmail.com>
2014-11-18 15:03:47 +1300

    Adds configurable interval and minutes.
    Can now specify schedules such as "Execute every 15 Minutes".
    Fix to ensure ExecuteInterval defaults to 1 if invalid.
    Add test that ExecuteInterval defaults to 1 if invalid or not specified, add test for minutes, add test that it reschedules at custom intervals.

--- tests/ScheduledExecutionTest.php
+++ tests/ScheduledExecutionTest.php
@@ -38,6 +38,76 @@ class ScheduledExecutionTest extends SapphireTest {
 		$this->assertNotEquals($test->ScheduledJobID, $jobId);
 		$this->assertEquals('EXECUTED', $test->Message);
 	}
+	
+	public function testScheduledExecutionInterval() {
+
+		$test = new TestScheduledDataObject;
+
+		$test->Title = 'Test execute at custom interval sizes';
+		$test->write();
+
+		$test->FirstExecution = '1980-09-22 09:15:00';
+		$test->ExecuteEvery = 'Minute';
+
+		$test->write();
+
+		// should now have a job
+		$this->assertTrue($test->ScheduledJobID > 0, 'Scheduled job has not been created');
+		// should default the ExecuteInterval
+		$this->assertEquals(1, $test->ExecuteInterval, 'ExecuteInterval did not default to 1');
+
+		// should check the interval in code also
+		$test->ExecuteInterval = 0;
+		$test->write();
+
+		$jobId = $test->ScheduledJobID;
+
+		// execute said job
+		$job = $test->ScheduledJob();
+		$job->execute();
+
+		// reload the test object and make sure its job has now changed
+		$test = DataObject::get_by_id('TestScheduledDataObject', $test->ID);
+
+		$this->assertNotEquals($test->ScheduledJobID, $jobId);
+		$this->assertEquals('EXECUTED', $test->Message);
+
+		$job = $test->ScheduledJob();
+
+		// should reschedule in 1 minute time
+		$expectedMinutes = date('i', time());
+		$expectedMinutes = intval($expectedMinutes, 10);
+		if ($expectedMinutes + 1 > 59) { // Wrap around the hour
+			$expectedMinutes = $expectedMinutes - 59;
+		}
+		$scheduledMinutes = substr($job->StartAfter, 14, 2);
+		$scheduledMinutes = intval($scheduledMinutes, 10);
+
+		$this->assertEquals($expectedMinutes + 1, $scheduledMinutes, 'Did not reschedule 1 minute later');
+
+		// test a custom interval of 3 minutes
+
+		$test->ExecuteInterval = 3;
+		$test->write();
+
+		$job = $test->ScheduledJob();
+		$job->execute();
+
+		$test = DataObject::get_by_id('TestScheduledDataObject', $test->ID);
+
+		$job = $test->ScheduledJob();
+
+		// should reschedule in 3 minutes time
+		$expectedMinutes = date('i', time());
+		$expectedMinutes = intval($expectedMinutes, 10);
+		if ($expectedMinutes + 3 > 59) {
+			$expectedMinutes = $expectedMinutes - 59;
+		}
+		$scheduledMinutes = substr($job->StartAfter, 14, 2);
+		$scheduledMinutes = intval($scheduledMinutes, 10);
+
+		$this->assertEquals($expectedMinutes + 3, $scheduledMinutes, 'Did not reschedule 3 minutes later');
+	}
 }
 
 
