4c3ca7b7f6ad09672830cd6c4a63ad56d9073947
matt-in-a-hat <matt@mediasuite.co.nz>
2017-05-16 11:03:49 +1200

    Fix markStarted not calculating timeout correctly

--- src/Services/QueuedJobService.php
+++ src/Services/QueuedJobService.php
@@ -713,7 +713,7 @@ class QueuedJobService
      */
     protected function markStarted()
     {
-        if ($this->startedAt) {
+        if (!$this->startedAt) {
             $this->startedAt = DBDatetime::now()->Format('U');
         }
     }
