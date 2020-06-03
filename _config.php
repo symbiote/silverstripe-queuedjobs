c91e8efd28508266893154340aeccdb919ffca00
jcarter <josh@symbiote.com.au>
2020-06-04 09:08:03 +1000

    chore(7.4): Replace Object with SS_Object

--- _config.php
+++ _config.php
@@ -1,5 +1,9 @@
 <?php
 
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 if (($QUEUED_JOBS_DIR = basename(dirname(__FILE__))) != 'queuedjobs') {
 	die("The queued jobs module must be installed in /queuedjobs, not $QUEUED_JOBS_DIR");
 }
