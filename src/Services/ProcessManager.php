6c5a5a4798cc1cc0c8c38c6099f78ae7bfe9acf1
Mojmir Fendek <mfendek@silverstripe.com>
2020-09-10 09:01:53 +1200

    PR fixes

--- src/Services/ProcessManager.php
+++ src/Services/ProcessManager.php
@@ -24,6 +24,14 @@ class ProcessManager extends BaseProcessManager
      */
     protected function getCommand($binary, $worker, $stdout, $stderr) // phpcs:ignore SlevomatCodingStandard.TypeHints
     {
+        // Nohup is short for “No Hangups.” It’s not a command that you run by itself.
+        // Nohup is a supplemental command that tells the Linux system not to stop another command once it has started.
+        // That means it’ll keep running until it’s done, even if the user that started it logs out
+        // Nohup is used here because the queue management process runs in a loop and it's starting new child processes
+        // which are running jobs
+        // It's possible to have the situation when queue management process terminates as it's no longer needed
+        // to create any new child processes but we still want the existing child processes to continue their work
+        // and finish the jbos which are already running
         return sprintf('nohup %s %s %%s %s %s & echo $!', $binary, $worker, $stdout, $stderr);
     }
 
