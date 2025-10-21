fd2a5100e0db9dd8d9eb22ec5ebb1a530d4d976c
Mojmir Fendek <mfendek@silverstripe.com>
2025-10-22 10:51:54 +1300

    Fixes.

---
+++ src/Dev/Job/QueueTestJob.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Symbiote\QueuedJobs\Dev\Job;
+
+use SilverStripe\ORM\FieldType\DBDatetime;
+use Symbiote\QueuedJobs\Dev\Service\QueueTestService;
+use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
+use Symbiote\QueuedJobs\Services\QueuedJob;
+
+/**
+ * Intended to support local development, unit tests but also debugging and refining queue settings in general
+ *
+ * @property int $randomID
+ * @property array $times
+ */
+class QueueTestJob extends AbstractQueuedJob
+{
+    private const int TOTAL_STEPS = 5;
+
+    public function hydrate(int $randomID): void
+    {
+        $this->randomID = $randomID;
+    }
+
+    public function getJobType(): int
+    {
+        return QueuedJob::QUEUED;
+    }
+
+    public function getTitle(): string
+    {
+        return 'Queue test job';
+    }
+
+    public function setup(): void
+    {
+        $this->times = [];
+        $this->totalSteps = self::TOTAL_STEPS;
+    }
+
+    /**
+     * Very basic process for testing jobs
+     *
+     * @return void
+     */
+    public function process(): void
+    {
+        $now = DBDatetime::now()->Rfc2822();
+
+        $times = $this->times;
+        $times[] = $now;
+        $this->times = $times;
+
+        $this->addMessage('Updated time to ' . $now);
+
+        // Wait a bit so we can simulate processing
+        QueueTestService::singleton()->sleep(1);
+
+        // make sure we're incrementing
+        $this->currentStep += 1;
+
+        // and checking whether we're complete
+        if ($this->currentStep < 5) {
+            return;
+        }
+
+        $this->isComplete = true;
+    }
+}
---
+++ src/Dev/Task/QueueTestTask.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Symbiote\QueuedJobs\Dev\Task;
+
+use SilverStripe\Core\Validation\ValidationException;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\PolyExecution\PolyOutput;
+use Symbiote\QueuedJobs\DataObjects\QueuedJobDescriptor;
+use Symbiote\QueuedJobs\Dev\Job\QueueTestJob;
+use Symbiote\QueuedJobs\Services\QueuedJob;
+use Symbiote\QueuedJobs\Services\QueuedJobService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Intended to support local development but also debugging and refining queue settings in general
+ */
+class QueueTestTask extends BuildTask
+{
+    protected static string $commandName = 'queue-test-jobs-task';
+
+    protected string $title = 'Generate test jobs task';
+
+    protected static string $description = 'Create test jobs based on the provided specification.';
+
+    public function getOptions(): array
+    {
+        $options = parent::getOptions();
+        $queueTypes = QueuedJobDescriptor::singleton()->getJobTypeValues();
+
+        $options[] = new InputOption(
+            'jobs',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Total number of jobs to create',
+            1
+        );
+
+        $options[] = new InputOption(
+            'queue',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Queue type to select which queue the created jobs will got to',
+            QueuedJob::QUEUED,
+            array_keys($queueTypes)
+        );
+
+        return $options;
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    protected function execute(InputInterface $input, PolyOutput $output): int
+    {
+        $service = QueuedJobService::singleton();
+        $total = $input->getOption('jobs');
+        $type = $input->getOption('queue');
+
+        $queueMap = QueuedJobDescriptor::singleton()->getJobTypeValues();
+
+        // Sanitise queue type and get human-readable label
+        $type = (string) $type;
+        $type = array_key_exists($type, $queueMap) ? $type : QueuedJob::QUEUED;
+        $queueLabel = $queueMap[$type];
+
+        for ($i = 0; $i < $total; $i += 1) {
+            $randomID = $i . date('U');
+            $randomID = (int) $randomID;
+
+            $job = new QueueTestJob();
+            $job->hydrate($randomID);
+            $jobID = $service->queueJob($job, null, null, $type);
+            $message = sprintf(
+                '"%s" inserted into "%s" queue (job ID = "%d")',
+                $job->getTitle(),
+                $queueLabel,
+                $jobID
+            );
+
+            echo $message . PHP_EOL;
+        }
+
+        return Command::SUCCESS;
+    }
+}
